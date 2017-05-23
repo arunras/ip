2c5fcefc96669451c079e1332212ad5e25615ade
Arun <prith000@citymail.cuny.edu>
2017-05-23 10:11:44 -0400

    insert is done

--- contact/routes.py
+++ contact/routes.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+# from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import *
 from functools import wraps
+import sqlite3
+
+DATABASE = 'account.db'
 
 app = Flask(__name__)
 
+app.config.from_object(__name__)
+
+
 app.secret_key = 'final exam'
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
 def home():
@@ -23,10 +33,16 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 
+
+
 @app.route('/hello')
 @login_required
 def hello():
-    return render_template('hello.html')
+    g.db = connect_db()
+    cur = g.db.execute('select name, number from tb_contact')
+    contact = [dict(name=row[0], number=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('hello.html', contact=contact)
 
 @app.route('/logout')
 def logout():
@@ -45,6 +61,25 @@ def login():
             return redirect(url_for('hello'))
     return render_template('login.html', error=error)
 
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    g.db = connect_db()
+    cur = g.db.execute('select name, number from tb_contact')
+    contact = [dict(name=row[0], number=row[1]) for row in cur.fetchall()]
+
+    error = None
+    if request.method == 'POST':
+        name = request.form['name']
+        number = request.form['number']
+        if name == '' or number == '':
+            error = 'Invalid! Please input name and number'
+        else:
+            g.db.execute('insert into tb_contact(id, name, number) values (?,?,?)',(2, name, number))
+            g.db.commit()
+            return redirect(url_for('contact'))
+    
+    g.db.close()
+    return render_template('contact.html', error=error, contact=contact)
 
 
 if __name__ == '__main__':
--- contact/venv/bin/activate.fish
+++ contact/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/arun/WorkSpace/ip/exam/proj1/venv"
+set -gx VIRTUAL_ENV "/Users/arun/WorkSpace/ip/exam/contact/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
